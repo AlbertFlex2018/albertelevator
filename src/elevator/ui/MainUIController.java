82bfa641df5a799345032472fb830463145fc0d9
Albert Flex <2662842460@qq.com>
2020-01-14 09:25:07 +0800

    add the ui impl.

---
+++ src/elevator/ui/MainUIController.java
@@ -0,0 +1,22 @@
+package elevator.ui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Admin
+ */
+public class MainUIController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
